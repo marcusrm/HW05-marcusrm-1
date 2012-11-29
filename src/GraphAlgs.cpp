5b8aa6f05bf005690b73090c88ae51a78c06f2b8
Richard Marcus <marcusrm@Muohio.edu>
2012-11-29 12:19:08 -0500

    commented out stupid stuff
    .

--- src/GraphAlgs.cpp
+++ src/GraphAlgs.cpp
@@ -6,49 +6,80 @@
 
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
 
-	EdgeWeight distanceTraveled = 0;
-	std::vector<NodeID>* path = new std::vector<NodeID>();
-	int nodesPassed = 0;
-	NWPair current;
-	double minInCol = DBL_MAX;
-	int* finished = new int[G->size()];
-	std::fill(finished,finished+ G->size()*sizeof(int), 0);
-	std::list<NWPair>::const_iterator iter;
-
-	std::list<NWPair> nearby = G->getAdj(0);
-	minInCol = (*(nearby.begin())).second;
-	for(iter = nearby.begin(); iter != nearby.end(); iter++){
-
-		if((*iter).second != 0 && (*iter).second < minInCol)
-			current = *iter;
-
-	}
-
-	finished[current.first] = 1;
-	distanceTraveled += current.second;
-	nodesPassed++;
-	path->push_back(current.first);
-
-	while(nodesPassed < G->size()){
-
-		std::list<NWPair> nearby = G->getAdj(current.first);
-		minInCol = (*(nearby.begin())).second;
-
-		for(iter = nearby.begin(); iter != nearby.end(); iter++){
-			if( (*iter).second != 0 && finished[(int)((*iter).second)] != 1 && (*iter).second < minInCol)
-				current = *iter;
-		}
-		
-		finished[current.first] = 1;
-		distanceTraveled += current.second;
-		nodesPassed++;
-		path->push_back(current.first);
-	}
 
 
-	std::pair<std::vector<NodeID>,EdgeWeight> result(*path,distanceTraveled);
-	
-	return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//EdgeWeight distanceTraveled = 0;
+	//std::vector<NodeID>* path = new std::vector<NodeID>();
+	//int nodesPassed = 0;
+	//NWPair current;
+	//double minInCol = DBL_MAX;
+	//int* finished = new int[G->size()];
+	//std::fill(finished,finished+ G->size()*sizeof(int), 0);
+	//std::list<NWPair>::const_iterator iter;
+
+	//std::list<NWPair> nearby = G->getAdj(0);
+	//minInCol = (*(nearby.begin())).second;
+	//for(iter = nearby.begin(); iter != nearby.end(); iter++){
+
+	//	if((*iter).second != 0 && (*iter).second < minInCol)
+	//		current = *iter;
+
+	//}
+
+	//finished[current.first] = 1;
+	//distanceTraveled += current.second;
+	//nodesPassed++;
+	//path->push_back(current.first);
+
+	//while(nodesPassed < G->size()){
+
+	//	std::list<NWPair> nearby = G->getAdj(current.first);
+	//	minInCol = (*(nearby.begin())).second;
+
+	//	for(iter = nearby.begin(); iter != nearby.end(); iter++){
+	//		if( (*iter).second != 0 && finished[(int)((*iter).second)] != 1 && (*iter).second < minInCol)
+	//			current = *iter;
+	//	}
+	//	
+	//	finished[current.first] = 1;
+	//	distanceTraveled += current.second;
+	//	nodesPassed++;
+	//	path->push_back(current.first);
+	//}
+
+
+	//std::pair<std::vector<NodeID>,EdgeWeight> result(*path,distanceTraveled);
+	//
+	//return result;
 }
 
 
